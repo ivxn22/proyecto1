bedc27111d1bd9286d47a0f10eaa4b7c837f3de0
Iván <Ivn@Ivn-PC>
2021-04-27 12:45:27 +0200

    Añadimos Principal

---
+++ prueba1/src/prueba1/principal.java
@@ -0,0 +1,9 @@
+package prueba1;
+
+public class principal {
+	public static void main(String[]args){
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Pruebas Git. Se añade cambio 1");
+	}
+}
